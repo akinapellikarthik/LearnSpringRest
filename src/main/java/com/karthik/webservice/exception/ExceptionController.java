90cf6e9e0a2430130b83e753c3493f115a5f954e
akarthik <akarthik@AKARTHIKJ6NM5H2.opentext.net>
2018-07-27 22:59:10 +0530

    changed ExceptionController to accept exception and UserNotFoundException

--- src/main/java/com/karthik/webservice/exception/ExceptionController.java
+++ src/main/java/com/karthik/webservice/exception/ExceptionController.java
@@ -1,21 +1,30 @@
 package com.karthik.webservice.exception;
 
+import java.util.Date;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @RestController
-public class ExceptionController extends ExceptionHandlerExceptionResolver {
-	
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+		ExceptionResponse res = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(res, HttpStatus.NOT_ACCEPTABLE);
+	}
+
+	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
 	public String getException() {
 		return "exception...";
 	}
 
-	
 }
