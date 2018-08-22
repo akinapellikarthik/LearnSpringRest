cb758985dc18d795db934f85b5bbc66771faef15
akarthik <akarthik@AKARTHIKJ6NM5H2.opentext.net>
2018-08-22 17:03:04 +0530

    commiting latest changes

--- src/main/java/com/karthik/webservice/entity/UserJPAController.java
+++ src/main/java/com/karthik/webservice/entity/UserJPAController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,12 @@ public class UserJPAController {
 	@GetMapping(path = "/findById/{id}")
 	public Optional<User> findOne(@PathVariable Long id) {
 		return userRepository.findById(id);
+	}
+	
+	@GetMapping(path="/requesparam-demo/{id}")
+	public void testRequestParamAndPathVar(@RequestParam(value="id1") long bookid1,@RequestParam long bookid2) {
+		System.out.println("Book id1 is:"+bookid1);
+		System.out.println("Book id2 is:"+bookid2);
+		
 	}
 }
